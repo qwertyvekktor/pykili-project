6fcf2fa1de65a9e46ecb2816479f67a133963af0
qwertyvekktor <55624769+qwertyvekktor@users.noreply.github.com>
2020-04-28 00:37:11 +0500

    Update pyproject.py

--- pyproject.py
+++ pyproject.py
@@ -2,20 +2,36 @@ from selenium import webdriver
 import selenium
 from bs4 import BeautifulSoup
 import requests
-import time
-#import re
-
-
+import re
+#multiproc для одновременного выполнения поиска пакета
 
 def iq_game(name):
+    w = []
+    w_1 = []
+    e = 1
     r = requests.get('https://iqga.me/base/')     #заходим на сайт
     soup = BeautifulSoup(r.content, 'html.parser')
     
-    links = soup.find_all("a")       #ищем <a>
-
-    for link in links:                #ищем ссылку на турнир(root_url)
-        if name in link:
-            root_url = 'https://iqga.me'+link.get("href")
+    links = soup.find_all("a") #ищем <a>
+    
+    while True:
+        h = 1
+        for link in links: #ищем ссылку на турнир(root_url)
+            if name in link:
+                root_url = 'https://iqga.me'+link.get("href")
+                e = 0
+            if 'Вперед' in link:     #проверка других страниц
+                new_url = 'https://iqga.me'+link.get("href")
+                h = 0
+        if e == 0:
+            break
+        r = requests.get(new_url)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        links = soup.find_all("a")
+        if h == 0:
+            continue
+        else:
+            return w
 
     r = requests.get(root_url)        #ищем вопросы по очереди
     soup = BeautifulSoup(r.content, 'html.parser')
@@ -24,6 +40,7 @@ def iq_game(name):
     a = ' '
     
     while f != 0:
+        u = 0
         r = requests.get(root_url)        #ищем вопросы по очереди
         soup = BeautifulSoup(r.content, 'html.parser')
         links = soup.find_all("a")
@@ -34,18 +51,39 @@ def iq_game(name):
             f = 0
 
         a = y
-        new_root = 'https://iqga.me'+y
+        new_root = 'https://iqga.me' + y
         qwest = requests.get(new_root)     #идем по ссылке вопроса  
         soup = BeautifulSoup(qwest.content, 'html.parser')
         t = soup.find_all("p")[3]
-        print(t, end='\n')
-        p = soup.find_all("p")[4]
+        if 'Раздаточный материал'in t.text:      #ищем ссылку на раздатку
+            o = soup.find_all("p")[4]
+            png_url = 'https://iqga.me' + o.a['href']
+            t = soup.find_all("p")[5]
+            p = soup.find_all("p")[6]
+            quest = re.search(r'(\[.+?\]\n)?(.+)', t.text)
+            w_1.append('Раздаточный материал')
+            w_1.append(png_url)
+            if quest.group(1) is True :
+                w_1.append(quest.group(1))
+            else:
+                w_1.append(quest.group(0))
+            w_1.append(p.text)
+            w.append(w_1)
+        else:
+            p = soup.find_all("p")[4]
+            quest = re.search(r'(\[.+?\]\n)?(.+)', t.text)
+            if quest.group(1) is True :
+                w_1.append(quest.group(1))
+            else:
+                w_1.append(quest.group(0))
+            w_1.append(p.text)
+            w.append(w_1)
         k += 1
-        print(p, end='\n')
-        
-    
+        w_1 = []
+    return w
+            
 def db_cgk(name):
-    driver = webdriver.Safari()                      #
+    driver = webdriver.Safari()                      #заходим на сайт бд чгк и вбиваем название пакета в поиск
     driver.get('https://db.chgk.info/search/tours')
     id_box = driver.find_element_by_name("keys")
     id_box.send_keys(name)
@@ -53,31 +91,21 @@ def db_cgk(name):
     search_button.click()
     time.sleep(7)
     url = driver.current_url
-    print(url)
+    #print(url)
 
-    answ = requests.get(url)                          #
+    answ = requests.get(url)                          #вытаскиваем ссылку на пакет
     driver.quit()
     soup = BeautifulSoup(answ.content, 'html.parser')
     links = soup.find_all("a")
     for link in links:
         if '/tour/' in link.get("href"):
             root_url = 'https://db.chgk.info' + link.get("href")
-            print(root_url)
+            #print(root_url)
 
 
-    
-    
-    
-    
-    
-
-
-
-
-
-    
-#iq_game('Лига вузов. V тур')
-db_cgk('Синхронный турнир «Вторая октава: Игра в стиле блюз»')
-
-
-    
+#def rand_q():
+#def rating():
+#def db_acc():
+ 
+q = iq_game('Лига Сибири. V тур')
+#db_cgk('Синхронный турнир «Вторая октава: Игра в стиле блюз»')
